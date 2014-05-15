283bf1f10a4df94e11c881dc72809caad0b96f5a
Jason Pirkey <jason@tave.com>
2014-05-14 23:30:57 -0400

    Use the PHP_OS predefined constant

--- lib/Sp_Lib.php
+++ lib/Sp_Lib.php
@@ -57,7 +57,7 @@ class Sp_Lib
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-    if (stripos(php_uname('s'),'Darwin') !== false) {
+    if (stripos(PHP_OS, 'Darwin') !== false) {
       curl_setopt($curl, CURLOPT_SSLVERSION, 3);
       curl_setopt($curl, CURLOPT_SSL_CIPHER_LIST, 'SSLv3');
     }
