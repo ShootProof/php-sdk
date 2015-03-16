2640f66b46114fa69eab35509bc4175e77fd947a
Brian DeShong <brian@shootproof.com>
2015-03-16 17:18:33 +0000

    Changed hard tabs to spaces.

--- lib/Sp_Lib.php
+++ lib/Sp_Lib.php
@@ -7,41 +7,41 @@
  */
 class Sp_Lib
 {
-	/**
-	 * Property to hold the version of the plugin
-	 *
-	 * @var string
-	 */
-	protected $_version = 'php-1.5.0';
+    /**
+     * Property to hold the version of the plugin
+     *
+     * @var string
+     */
+    protected $_version = 'php-1.5.0';
 
-	/**
-	 * Wrapper method to make an api request
-	 *
-	 * @param $code auth code
-	 * @return json response with access/refresh token
-	 */
-	protected function _makeApiRequest($params, $photos = array())
-	{
-		$token = array('access_token' => $this->_getAccessToken());
+    /**
+     * Wrapper method to make an api request
+     *
+     * @param $code auth code
+     * @return json response with access/refresh token
+     */
+    protected function _makeApiRequest($params, $photos = array())
+    {
+        $token = array('access_token' => $this->_getAccessToken());
 
-		$params = array_merge($params, $token);
+        $params = array_merge($params, $token);
 
-		return $this->_makeRequest($this->_baseEndPoint, $params, $photos);
-	}
+        return $this->_makeRequest($this->_baseEndPoint, $params, $photos);
+    }
 
-	/**
-	 * Method to make the request to the API
-	 *
-	 * @param array $params
-	 * @param array $photos
-	 * @return array
-	 */
-	protected function _makeRequest($url, $params = array(), $photos = array())
-	{
-		// if any photos exist, let's merge them into the params
-		if (count($photos)) {
-			$params = array_merge($params, $photos);
-		}
+    /**
+     * Method to make the request to the API
+     *
+     * @param array $params
+     * @param array $photos
+     * @return array
+     */
+    protected function _makeRequest($url, $params = array(), $photos = array())
+    {
+        // if any photos exist, let's merge them into the params
+        if (count($photos)) {
+            $params = array_merge($params, $photos);
+        }
 
         foreach ($params as $key => $value) {
             if (is_array($value)) {
@@ -51,63 +51,63 @@ class Sp_Lib
             }
         }
 
-		// send along some headers for reporting
-		$headers = array(
-			'X-WRAPPER: ' . $this->_version
-		);
+        // send along some headers for reporting
+        $headers = array(
+            'X-WRAPPER: ' . $this->_version
+        );
 
-		// init curl to make the request
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_POST, true);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        // init curl to make the request
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-		// the API response
-		$response = curl_exec($curl);
-		$info = curl_getinfo($curl);
-		$error = curl_error($curl);
-		$errno = curl_errno($curl);
+        // the API response
+        $response = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        $error = curl_error($curl);
+        $errno = curl_errno($curl);
 
-		if ($error) {
-			throw new Sp_CurlException($error, $errno);
-		}
+        if ($error) {
+            throw new Sp_CurlException($error, $errno);
+        }
 
-		if (!$response) {
-			throw new Sp_NoResponseException('The API did not return any response. Error #' . $info['http_code']);
-		}
+        if (!$response) {
+            throw new Sp_NoResponseException('The API did not return any response. Error #' . $info['http_code']);
+        }
 
-		$json = json_decode($response, true);
+        $json = json_decode($response, true);
 
-		if ($json['stat'] != 'ok') {
-			throw new Sp_Exception(
+        if ($json['stat'] != 'ok') {
+            throw new Sp_Exception(
                 'The API did not return an OK response. ' . $json['msg'],
                 0,
                 $response
             );
-		}
+        }
 
-		return $json;
-	}
+        return $json;
+    }
 
-	/**
-	 * Build the URL for given path and parameters.
-	 *
-	 * @param $path
-	 *   (optional) The path.
-	 * @param $params
-	 *   (optional) The query parameters in associative array.
-	 *
-	 * @return
-	 *   The URL for the given parameters.
-	 */
-	protected function _getUri($path = '', $params = array()) {
-		$url = $path;
-		$url .= '?' . http_build_query($params, NULL, '&');
-		return $url;
-	}
+    /**
+     * Build the URL for given path and parameters.
+     *
+     * @param $path
+     *   (optional) The path.
+     * @param $params
+     *   (optional) The query parameters in associative array.
+     *
+     * @return
+     *   The URL for the given parameters.
+     */
+    protected function _getUri($path = '', $params = array()) {
+        $url = $path;
+        $url .= '?' . http_build_query($params, NULL, '&');
+        return $url;
+    }
 
     /**
      * Adapted from http://stackoverflow.com/a/8224117
